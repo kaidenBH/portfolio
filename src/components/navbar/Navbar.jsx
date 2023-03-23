626342c7f71cb98f4a119febaf4dbdd5e9ab9a9c
yasuoPi <haitem.bourahla7@gmail.com>
2023-03-23 17:42:37 +0100

    finished the navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,20 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
 import{ RiMenu3Line, RiCloseLine } from 'react-icons/ri';
-import logo from '../../assets/logo.svg'
-import './navbar.css'
+import logo from '../../assets/logo.svg';
+import './navbar.css';
+const Menu = () => (
+  <>
+    <p><a href="#portfolio">Portfolio</a></p>
+    <p><a href="#Haitem">About me</a></p>
+    <p><a href="#contact">Contact me</a></p>
+  </>
+)
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
     <div className='haitem_navbar'>
-        <div className='haitem_navbar-links'>
-          <div className='haitem_navbar-links_logo'>
-            <img src={logo} alt="logo"/>
-          </div>
-          <div className='haitem_navbar-links_container'>
-            <p><a href="#portfolio">Portfolio</a></p>
-            <p><a href="#Haitem">About me</a></p>
-            <p><a href="#contact">Contact me</a></p>
-          </div>
+      <div className='haitem_navbar-links'>
+        <div className='haitem_navbar-links_logo'>
+          <img src={logo} alt="logo"/>
         </div>
+        <div className='haitem_navbar-links_container'>
+          <Menu /> 
+        </div>
+      </div>
+      <div className='haitem_navbar-menu'>
+        {toggleMenu
+          ?<RiCloseLine color='#fff' size={27} onClick={ () => setToggleMenu(false)} />
+          :<RiMenu3Line color='#fff' size={27} onClick={ () => setToggleMenu(true)} />  
+        }
+        {toggleMenu && (
+          <div className='haitem_navbar-menu_container scale-up-center'>
+            <div className='haitem_navbar-menu_container-links'>
+              <Menu />
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
