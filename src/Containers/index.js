c80b2a54d97ab71296c2728e685911cd9cc7ca54
yasuoPi <haitem.bourahla7@gmail.com>
2023-03-25 19:55:30 +0100

    Updates

--- src/Containers/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Contact } from './contact/Contact';
-export { default as Portfolio } from './portfolio/Portfolio';
-export { default as Footer } from './footer/Footer';
-export { default as Haitem } from './Haitem/Haitem';
-export { default as Header } from './header/Header';
