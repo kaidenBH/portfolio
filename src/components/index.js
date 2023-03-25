c0cbe844decdb6563356db35f087aafe80196eb4
yasuoPi <haitem.bourahla7@gmail.com>
2023-03-25 20:21:09 +0100

    more bug fixed

--- src/components/index.js
+++ src/components/index.js
@@ -3,4 +3,3 @@ export { default as Cta } from './cta/Cta';
 export { default as Feature } from './feature/Feature';
 export { default as Navbar } from './navbar/Navbar';
 export { default as Chart } from './chart/Chart';
-export { default as Background } from './background/Background';
