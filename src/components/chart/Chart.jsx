865e2c62b517f0b8f0cfe505c8750bf7e0db78c3
yasuoPi <haitem.bourahla7@gmail.com>
2023-03-25 16:47:33 +0100

    fixed a lot of bugs

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -2,29 +2,29 @@ import React from 'react'
 import './chart.css'
 const Chart = () => {
   return (
-    <div class="skills">
-        <div class="progressBar">
+    <div className="skills">
+        <div className="progressBar">
             <h4>HTML5, CSS3</h4>
-            <div class="progressBarContainer">
-                <div class="progressBarValue value-100"></div>
+            <div className="progressBarContainer">
+                <div className="progressBarValue value-100"></div>
             </div>
         </div>
-        <div class="progressBar">
+        <div className="progressBar">
             <h4>Python, Java</h4>
-            <div class="progressBarContainer">
-                <div class="progressBarValue value-90"></div>
+            <div className="progressBarContainer">
+                <div className="progressBarValue value-90"></div>
             </div>
         </div>
-        <div class="progressBar">
+        <div className="progressBar">
             <h4>Javascript, React</h4>
-            <div class="progressBarContainer">
-                <div class="progressBarValue value-70"></div>
+            <div className="progressBarContainer">
+                <div className="progressBarValue value-70"></div>
             </div>
         </div>
-        <div class="progressBar">
+        <div className="progressBar">
             <h4>C#</h4>
-            <div class="progressBarContainer">
-                <div class="progressBarValue value-80"></div>
+            <div className="progressBarContainer">
+                <div className="progressBarValue value-80"></div>
             </div>
         </div>
     </div>
