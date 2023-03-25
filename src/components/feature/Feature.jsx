8a3650091a2ff1acd9fa38be59c95f5e7c2f07b7
yasuoPi <haitem.bourahla7@gmail.com>
2023-03-25 19:11:22 +0100

    finished the about me section

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import './feature.css'
-const Feature = () => {
+const Feature = ({title,text}) => {
   return (
-    <div>
-        feature
+    <div className='haitem_feature-container_feature'>
+        <div className='haitem_feature-container_feature-title'>
+          <div />
+          <h1>{title}</h1>
+        </div>
+        <div className='haitem_feature-container_feature-text'>
+          <p>{text}</p>
+        </div>
     </div>
   )
 }
